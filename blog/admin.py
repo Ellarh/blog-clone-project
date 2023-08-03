8762a1b1dcb263b1a0fc3fc8a981ba41da7a0002
Ellarh <ellamoves01@gmail.com>
2023-08-03 14:44:42 +0100

    Added new functionality to the Admin Page

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Comments, Post
 
-# Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    # fields = ['text', 'title', 'author'] This is for ordering the fields in the model
+    search_fields = ['title']
+    list_filter = ['author', 'published_date', ]
+    list_display = ['author', 'title', 'published_date']
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comments)
+
+
+
